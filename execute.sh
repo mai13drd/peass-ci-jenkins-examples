a9ed79674562f4e4b0725c2dea2c413bdca6cd57
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-02-17 16:53:03 +0100

    seed-job and example-job added.

--- execute.sh
+++ execute.sh
@@ -5,11 +5,14 @@ cd ../slave
 docker build -t slave .
 
 cd ../..
-cp files/master/casc.yaml jenkins_master_home/
+cp files/master/casc.yaml jenkins_master_home
+
+mkdir -p jenkins_master_home/jobs/seed-job/workspace/dslScripts
+
+cp files/master/seed-job/config.xml jenkins_master_home/jobs/seed-job
+cp files/master/dslScripts/helloWorld.groovy jenkins_master_home/jobs/seed-job/workspace/dslScripts
 
 docker run -d --name jenkins_master --rm --publish 8080:8080 --volume $(pwd)/jenkins_master_home:/var/jenkins_home \
 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=123 jenkins_master
 
 docker run -d --name agent-1 --rm --volume $(pwd)/jenkins_slave-1_home:/var/jenkins_home slave
-
-
