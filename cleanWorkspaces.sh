5285f2e0db04376243a19ce6ec55b9c09850882f
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-02-18 08:29:54 +0100

    Added cleanWorkspaces.sh and testExecute.sh.

---
+++ cleanWorkspaces.sh
@@ -0,0 +1,2 @@
+rm -rf jenkins_master_home/{*,.*}
+rm -rf jenkins_slave-1_home/{*,.*}
---
+++ testExecute.sh
@@ -0,0 +1,11 @@
+#This is just for testing.
+
+cp files/master/casc.yaml jenkins_master_home
+
+mkdir -p jenkins_master_home/jobs/seed-job/workspace/dslScripts
+
+cp files/master/seed-job/config.xml jenkins_master_home/jobs/seed-job
+cp files/master/dslScripts/helloWorld.groovy jenkins_master_home/jobs/seed-job/workspace/dslScripts
+
+docker run -d --name jenkins_master --rm --publish 8080:8080 --volume $(pwd)/jenkins_master_home:/var/jenkins_home \
+--env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=123 jenkins_master
