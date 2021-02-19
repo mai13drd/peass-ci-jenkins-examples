6eae9be7b8c6f9702c78aadbd6fd6b04d80d27f1
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-02-19 11:08:11 +0100

    Changed cleanWorkspaces.sh

--- cleanWorkspaces.sh
+++ cleanWorkspaces.sh
@@ -1,2 +1,11 @@
 rm -rf jenkins_master_home/{*,.*}
 rm -rf jenkins_slave-1_home/{*,.*}
+
+cd jenkins_master_home
+
+cat << EOF > .gitignore
+*
+!.gitignore
+EOF
+
+cp .gitignore ../jenkins_slave-1_home
