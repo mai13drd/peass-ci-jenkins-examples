95fa6adcd8fe1376be31518a48fb3104762f1d3c
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-02-19 15:30:11 +0100

    Copying credentials at master-startup. ssh-connections works.

--- createNode.sh
+++ createNode.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-     
-#cat <<EOF | java -jar jenkins-cli.jar -s $1 create-node $2
+
+IP=$1
 
 cat <<EOF | java -jar jenkins-cli.jar -s "http://localhost:8080" -auth admin:123 create-node agent-1
 <slave>
-  <name>'agent-1'</name>
+  <name>agent-1</name>
   <description></description>
-  <remoteFS>/home/ubuntu</remoteFS>
+  <remoteFS>/home/ubuntu/jenkins_home</remoteFS>
   <numExecutors>1</numExecutors>
   <mode>NORMAL</mode>
   <retentionStrategy class="hudson.slaves.RetentionStrategy$Always"/>
   <launcher class="hudson.plugins.sshslaves.SSHLauncher" plugin="ssh-slaves@1.31.5">
-    <host>172.17.0.2</host>
+    <host>$1</host>
     <port>22</port>
-    <credentialsId>68791c3d-8d51-4f75-8dd8-67014ad7a963</credentialsId>
+    <credentialsId>slave-credentials</credentialsId>
     <launchTimeoutSeconds>60</launchTimeoutSeconds>
     <maxNumRetries>10</maxNumRetries>
     <retryWaitTime>15</retryWaitTime>
@@ -22,6 +22,5 @@ cat <<EOF | java -jar jenkins-cli.jar -s "http://localhost:8080" -auth admin:123
   </launcher>
   <label></label>
   <nodeProperties/>
-  <userId>22</userId>
 </slave>
 EOF
--- testExecute.sh
+++ testExecute.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #This is just for testing.
 
 cp files/master/casc.yaml jenkins_master_home
@@ -6,11 +8,16 @@ mkdir -p jenkins_master_home/jobs/seed-job/workspace/dslScripts
 
 cp files/master/seed-job/config.xml jenkins_master_home/jobs/seed-job
 cp files/master/dslScripts/helloWorld.groovy jenkins_master_home/jobs/seed-job/workspace/dslScripts
-cp files/master/dslScripts/scriptApproval.xml jenkins_master_home
 
-#docker run -d --name agent-1 --rm --volume $(pwd)/jenkins_slave-1_home:/var/jenkins_home slave
+cp files/master/dslScripts/scriptApproval.xml jenkins_master_home
+cp files/master/credentials.xml jenkins_master_home
+
+docker run -d --name agent-1 --rm --volume $(pwd)/jenkins_slave-1_home:/home/ubuntu/jenkins_home slave
+echo "IP of slave is: $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' agent-1)"
 
 docker run -d --name jenkins_master --rm --publish 8080:8080 --volume $(pwd)/jenkins_master_home:/var/jenkins_home \
---env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=123 jenkins_master
+    --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=123 jenkins_master
+
+#bash ./createNode.sh
 
 
