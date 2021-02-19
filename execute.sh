75e583b419e739a2bf4bbd0c93b1e5d47f58f040
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-02-19 12:44:45 +0100

    Using same directories in jenkins and slave-container.

--- execute.sh
+++ execute.sh
@@ -14,7 +14,7 @@ cp files/master/dslScripts/helloWorld.groovy jenkins_master_home/jobs/seed-job/w
 
 cp files/master/dslScripts/scriptApproval.xml jenkins_master_home
 
-docker run -d --name agent-1 --rm --volume $(pwd)/jenkins_slave-1_home:/var/jenkins_home slave
+docker run -d --name agent-1 --rm --volume $(pwd)/jenkins_slave-1_home:/home/ubuntu/jenkins_home slave
 
 docker run -d --name jenkins_master --rm --publish 8080:8080 --volume $(pwd)/jenkins_master_home:/var/jenkins_home \
     --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=123 jenkins_master
