dee17e729b3a3362b3fff58cdf66ed86cbadfa35
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-02-22 11:17:08 +0100

    Building peass on agent-1. Build is triggered when agent-1 is online.

--- files/master/dslScripts/helloWorld.groovy
+++ files/master/dslScripts/helloWorld.groovy
@@ -1,9 +1,22 @@
 job('example') {
 
+  scm {
+        github('dagere/peass', 'master')
+    }
+
   label('agent-1')
 
+  triggers {
+             hudsonStartupTrigger {
+               quietPeriod('3')
+               runOnChoice('ON_CONNECT')
+               label('agent-1')
+               nodeParameterName('')
+             }
+  }
+
   steps {
-    shell('echo Hello World!')
+    maven('clean install -DskipTests')
   }
 
 }
