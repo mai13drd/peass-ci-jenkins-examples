e58e7ac311957bb6b78d28acd8790c677cfc7102
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-03-30 15:14:45 +0200

    Added waitForBuildEndAndCheckResults.sh

---
+++ scripts/waitForBuildEndAndCheckResults.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Give jenkins some time to startup and start building
+sleep 15
+
+building=true
+while [ building ]
+do
+	sleep 5
+	building=$(echo 'println(jenkins.model.Jenkins.instance''.getItem("demo-pipeline_masterOnly").lastBuild.building)' | java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:123 groovy =)
+	echo 'Jenkins is still building...'
+done
+
+./checkResults.sh
