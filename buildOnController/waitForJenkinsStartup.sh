3fe3ddeaf1541e4f0457b048ac6605b71990a184
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-04-20 11:46:06 +0200

    Added waitForJenkinsStartup.sh

---
+++ buildOnController/waitForJenkinsStartup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+online=1
+while [ $online -ne 0 ]
+do
+	echo 'Waiting for jenkins startup...'
+    sleep 2
+	{ echo 'println(jenkins.model.Jenkins.instance''.getItem("buildOnAgentManually"))' | java -jar ../common/jenkins-cli.jar -s http://localhost:8080 -auth admin:123 groovy =; } 2> /dev/null
+    online=$?
+done
+
+echo "------------------------------------"
+echo "Jenkins is fully started."
+echo "------------------------------------"
