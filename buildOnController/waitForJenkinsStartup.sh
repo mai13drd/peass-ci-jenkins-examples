021004ea7858d2197621420b56de7936e076f083
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-04-20 15:57:49 +0200

    Try to parameterize calls of jenkins-cli.jar

--- buildOnController/waitForJenkinsStartup.sh
+++ buildOnController/waitForJenkinsStartup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-#set -e
+set -e
 
-online=1
-while [ $online -ne 0 ]
-do
-	echo 'Waiting for jenkins startup...'
-    sleep 2
-	{ echo 'println(jenkins.model.Jenkins.instance''.getItem("buildOnAgentManually"))' | java -jar ../common/jenkins-cli.jar -s http://localhost:8080 -auth admin:123 groovy =; } 2> /dev/null
-    online=$?
-done
+source ../common/functions.sh
 
-echo "------------------------------------"
-echo "Jenkins is fully started."
-echo "------------------------------------"
+waitForJenkinsStartup "buildOnAgentManually"
