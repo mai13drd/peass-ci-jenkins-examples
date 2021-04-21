5e4d223ed9a5344216c631e41b20e520ecfa2235
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-04-21 13:30:35 +0200

    Added waitForBuildEnd to functions.sh

--- buildOnController/waitForBuildEnd.sh
+++ buildOnController/waitForBuildEnd.sh
@@ -2,14 +2,5 @@
 
 set -e
 
-building=true
-while [ "$building" = true ]
-do
-    sleep 5
-	building=$(echo 'println(jenkins.model.Jenkins.instance''.getItem("buildOnController").lastBuild.building)' | java -jar ../common/jenkins-cli.jar -s http://localhost:8080 -auth admin:123 groovy =)
-    echo 'Jenkins is still building...'
-done
-
-echo "------------------------------------"
-echo 'Jenkins finished building.'
-echo "------------------------------------"
+source ../common/functions.sh
+waitForBuildEnd "buildOnAgentManually"
--- common/functions.sh
+++ common/functions.sh
@@ -3,7 +3,8 @@ waitForJenkinsStartup () {
     #Avoid exit on error if jenkins is not started yet
     set +e
 
-    command="{ echo 'println(jenkins.model.Jenkins.instance''.getItem(\"$1\"))' | java -jar ../common/jenkins-cli.jar -s http://localhost:8080 -auth admin:123 groovy =; } &>/dev/null"
+    command="{ echo 'println(jenkins.model.Jenkins.instance''.getItem(\"$1\"))' | java -jar ../common/jenkins-cli.jar -s \
+        http://localhost:8080 -auth admin:123 groovy =; } &>/dev/null"
 
     online=1
     while [ $online -ne 0 ]
@@ -18,5 +19,23 @@ waitForJenkinsStartup () {
 
     echo "------------------------------------"
     echo "Jenkins is fully started."
+    echo "------------------------------------"
+}
+
+waitForBuildEnd () {
+
+    command="echo 'println(jenkins.model.Jenkins.instance''.getItem(\"$1\").lastBuild.building)' | java -jar ../common/jenkins-cli.jar -s \
+            http://localhost:8080 -auth admin:123 groovy ="
+
+    building=true
+    while [ "$building" = true ]
+    do
+        sleep 5
+        building=eval command
+        echo 'Jenkins is still building...'
+    done
+
+    echo "------------------------------------"
+    echo 'Jenkins finished building.'
     echo "------------------------------------"
 }
