3ced606c804d9483ba7d68dd93b890d10df90fdc
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-04-27 10:45:03 +0200

    Changed workflow files. Separated main branch.

--- buildOnAgentManually/buildWaitCheckResults.sh
+++ buildOnAgentManually/buildWaitCheckResults.sh
@@ -3,7 +3,5 @@
 ./buildOnAgentManually.sh
 ./waitForJenkinsStartup.sh
 ./registerAgentToController.sh
-#wait some time until jenkins_agent-1 is online and build is started
-sleep 10
 ./waitForBuildEnd.sh
 ./checkResults.sh
--- buildOnAgentManually/waitForBuildEnd.sh
+++ buildOnAgentManually/waitForBuildEnd.sh
@@ -3,4 +3,6 @@
 set -e
 
 source ../common/functions.sh
+#wait some time until jenkins_agent-1 is online and build is started
+sleep 10
 waitForBuildEnd "buildOnAgentManually"
