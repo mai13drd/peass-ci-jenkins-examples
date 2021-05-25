b2a73510e314c3b91837d82894cd9001cc1ec0e5
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-05-25 11:26:08 +0200

    Only build necessary peass projects

--- buildOnController/buildPeassAndPeassCI.sh
+++ buildOnController/buildPeassAndPeassCI.sh
@@ -6,7 +6,7 @@ cd ../common
 
 git clone https://github.com/dagere/peass && \
     cd peass && \
-    mvn clean install -DskipTests
+    mvn clean install -pl dependency,measurement,analysis -DskipTests
 
 cd ..
 
