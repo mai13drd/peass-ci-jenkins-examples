edad89d9c0ecadefa1b43c9de9787867117ce991
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-05-27 10:33:41 +0200

    Cloning local demo-project for builOnAgentAutomatic

--- buildOnAgentAutomatic/buildOnAgentAutomatic.sh
+++ buildOnAgentAutomatic/buildOnAgentAutomatic.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -e
+
 docker build -t jenkins_controller ../common/controller
 
 cp ../common/controller/casc.yaml ../jenkins_controller_home
 
 mkdir -p ../jenkins_controller_home/plugins
-cp ../common/peass-ci.hpi ../jenkins_controller_home/plugins
+cp ../common/peass-ci/target/peass-ci.hpi ../jenkins_controller_home/plugins
 
 mkdir -p ../jenkins_controller_home/jobs/buildOnAgentAutomatic
 cp config.xml ../jenkins_controller_home/jobs/buildOnAgentAutomatic
+
+tar -xf ../common/demo-project.tar.xz --directory ../jenkins_controller_home/jobs/buildOnAgentAutomatic
 
 docker run -d --name jenkins_controller --rm --publish 8080:8080 \
     --volume $(pwd)/../jenkins_controller_home:/var/jenkins_home \
