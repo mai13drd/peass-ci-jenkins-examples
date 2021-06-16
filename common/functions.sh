a8679983d86286ad4328d85ad90bf6aff5df06ad
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-06-16 12:00:34 +0200

    Removed all other checks, cat ExampleTest_test.js

--- common/functions.sh
+++ common/functions.sh
@@ -56,46 +56,6 @@ checkResults () {
 
     VERSION="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
 
-    INITIALVERSION="f2de60284ff832d5232870da6ace172ab1361eb7"
-    INITIAL_SELECTED=$(grep "initialversion" -A 1 $DEPENDENCY_FILE | grep "\"version\"" | tr -d " \"," | awk -F':' '{print $2}')
-    if [ "$INITIAL_SELECTED" != "$INITIALVERSION" ]
-    then
-	    echo "Initialversion should be $INITIALVERSION, but was $INITIAL_SELECTED"
-        exit 1
-    fi
-
-    if [ ! -f $EXECUTION_FILE ]
-    then
-        echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-        echo "$EXECUTION_FILE could not be found!"
-        echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-        exit 1
-    fi
-
-    #Check, if $CHANGES_DEMO_PROJECT contains the correct commit-SHA
-    TEST_SHA=$(grep -A1 'versionChanges" : {' $CHANGES_DEMO_PROJECT | grep -v '"versionChanges' | grep -Po '"\K.*(?=")')
-    if [ "$VERSION" != "$TEST_SHA" ]
-    then
-        echo "commit-SHA ("$VERSION") is not equal to the SHA in $CHANGES_DEMO_PROJECT ("$TEST_SHA")!"
-	    cat $CHANGES_DEMO_PROJECT
-	    exit 1
-    else
-	    echo "$CHANGES_DEMO_PROJECT contains the correct commit-SHA."
-    fi
-
-    #Check, if a slowdown is detected for innerMethod
-    STATE=$(grep -A21 '"call" : "de.dagere.peass.Callee#innerMethod",' $PEASS_DATA/visualization/$VERSION/de.dagere.peass.ExampleTest_test.js \
-        | grep '"state" : "SLOWER",' \
-        | grep -o 'SLOWER')
-    if [ "$STATE" != "SLOWER" ]
-    then
-        echo "State for Callee#innerMethod in de.dagere.peass.ExampleTest_test.js has not the expected value SLOWER, but was $STATE!"
-        cat $PEASS_DATA/visualization/$VERSION/de.dagere.peass.ExampleTest_test.js
-	    exit 1
-    else
-	    echo "Slowdown is detected for innerMethod."
-    fi
-
 #var source in de.dagere.peass.ExampleTest_test.js is empty, so this fails!
     SOURCE_METHOD_LINE=$(grep "Callee.method1_" $PEASS_DATA/visualization/$VERSION/de.dagere.peass.ExampleTest_test.js -A 3 \
         | head -n 3 \
@@ -104,6 +64,9 @@ checkResults () {
     then
         echo "Line could not be detected - source reading probably failed."
         echo "SOURCE_METHOD_LINE: $SOURCE_METHOD_LINE"
+        echo "------------------------------------"
+        cat $PEASS_DATA/visualization/$VERSION/de.dagere.peass.ExampleTest_test.js
+        echo "------------------------------------"
         exit 1
     else
         echo "SOURCE_METHOD_LINE is correct."
