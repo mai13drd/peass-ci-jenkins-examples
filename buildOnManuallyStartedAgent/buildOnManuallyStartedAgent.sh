7b43fb3aaab597c8af72a667dc9990e36f606a55
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-06-17 11:25:51 +0200

    Build and copy peass(-ci) and demo-project, waitForBuildEnd

--- buildOnManuallyStartedAgent/buildOnManuallyStartedAgent.sh
+++ buildOnManuallyStartedAgent/buildOnManuallyStartedAgent.sh
@@ -8,13 +8,13 @@ docker build -t jenkins_agent ../common/agent
 cp ../common/controller/casc.yaml ../jenkins_controller_home
 cp credentials.xml ../jenkins_controller_home/
 
-#mkdir -p ../jenkins_controller_home/plugins
-#cp ../common/peass-ci/target/peass-ci.hpi ../jenkins_controller_home/plugins
+mkdir -p ../jenkins_controller_home/plugins
+cp ../common/peass-ci/target/peass-ci.hpi ../jenkins_controller_home/plugins
 
 mkdir -p ../jenkins_controller_home/jobs/buildOnManuallyStartedAgent
 cp config.xml ../jenkins_controller_home/jobs/buildOnManuallyStartedAgent
 
-#tar -xf ../common/demo-project.tar.xz --directory ../jenkins_agent-1_home
+tar -xf ../common/demo-project.tar.xz --directory ../jenkins_agent-1_home
 
 docker run -d --name jenkins_agent-1 --rm \
     --volume $(pwd)/../jenkins_agent-1_home:/home/ubuntu/jenkins_home jenkins_agent
