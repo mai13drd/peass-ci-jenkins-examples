3f69a8b3e0c8a5c6acdfe76aae0338b9d1357a3c
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-06-17 11:17:50 +0200

    Build, start, register agent

--- buildOnManuallyStartedAgent/buildOnManuallyStartedAgent.sh
+++ buildOnManuallyStartedAgent/buildOnManuallyStartedAgent.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker build -t jenkins_controller ../common/controller
-#docker build -t jenkins_agent ../common/agent
+docker build -t jenkins_agent ../common/agent
 
 cp ../common/controller/casc.yaml ../jenkins_controller_home
 cp credentials.xml ../jenkins_controller_home/
@@ -16,13 +16,13 @@ cp config.xml ../jenkins_controller_home/jobs/buildOnManuallyStartedAgent
 
 #tar -xf ../common/demo-project.tar.xz --directory ../jenkins_agent-1_home
 
-#docker run -d --name jenkins_agent-1 --rm \
-#    --volume $(pwd)/../jenkins_agent-1_home:/home/ubuntu/jenkins_home jenkins_agent
+docker run -d --name jenkins_agent-1 --rm \
+    --volume $(pwd)/../jenkins_agent-1_home:/home/ubuntu/jenkins_home jenkins_agent
 
-#AGENT_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins_agent-1)
-#echo "------------------------------------"
-#echo "IP of jenkins_agent-1 is: $AGENT_IP"
-#echo "------------------------------------"
+AGENT_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins_agent-1)
+echo "------------------------------------"
+echo "IP of jenkins_agent-1 is: $AGENT_IP"
+echo "------------------------------------"
 
 docker run -d --name jenkins_controller --rm --publish 8080:8080 \
     --volume $(pwd)/../jenkins_controller_home:/var/jenkins_home \
