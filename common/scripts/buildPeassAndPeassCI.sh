57aaee13a1f9827b15e279eac6def52a44c3f56a
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-06-21 12:32:44 +0200

    peass (01.06. f67a1ea0) peass-ci (01.06. e15f7ed)

--- common/scripts/buildPeassAndPeassCI.sh
+++ common/scripts/buildPeassAndPeassCI.sh
@@ -6,13 +6,13 @@ cd ../common
 
 git clone https://github.com/dagere/peass && \
     cd peass && \
-    git reset --hard 87a99a81 && \
+    git reset --hard f67a1ea0 && \
     ./mvnw clean install -pl dependency,measurement,analysis -DskipTests
 
 cd ..
 
 git clone https://github.com/dagere/peass-ci && \
     cd peass-ci && \
-    git reset --hard 9b9f90e && \
+    git reset --hard e15f7ed && \
     ./mvnw clean -B package --file pom.xml -DskipTests
 
